1e6891c8af760eda5715f55c3feaa1bcb95f1ca3
radoslav <radixko@gmail.com>
2017-11-15 19:57:15 +0100

    Map view and location

--- Pokemon go/ViewController.swift
+++ Pokemon go/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager.requestWhenInUseAuthorization()
+        } else {
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else{
+            manager.stopUpdatingLocation() 
+        }
+        
     }
-
-
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate{
+            let region = MKCoordinateRegionMakeWithDistance(coord, 1000, 1000)
+            mapView.setRegion(region, animated: false)
+        }
+    }
 }
 
